bc0906cd97f4b8abb263ea600f434646b1abafd8
apekshabhikavarkhede <apekshavarkhede1508@gmail.com>
2019-10-03 19:43:33 +0530

    [Apeksha|Sushant]Add. cosumer to park vehicles

---
+++ src/main/java/com/thoughtworks/Consumer.java
@@ -0,0 +1,48 @@
+package com.thoughtworks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consumer {
+    private final List<Object> vehicleToPark;
+
+    public Consumer(List<Object> vehicleToPark) {
+        this.vehicleToPark = vehicleToPark;
+    }
+
+    private void parkVechile(ParkingLot parkingLot, ParkingLot parkingLot1) {
+
+//        for (int i = 0; i < vehicleToPark.size(); i++)
+//        {
+//            System.out.println( parkingLot.park( vehicleToPark.get(i)) +"parkingLot" + i );
+//            if( (parkingLot.park( vehicleToPark.get(i)).equals("Space Not Available") || parkingLot.park(vehicleToPark.get(i)).equals("Already Parked")) )
+//            {
+//                System.out.println( parkingLot1.park(vehicleToPark.get(i)) +"parkingLot1" );
+//            }
+//        }
+
+
+        int i= 0;
+        do
+        {
+            System.out.println( parkingLot.park( vehicleToPark.get(i)) +"parkingLot" + i );
+            i++;
+        }while(i < vehicleToPark.size());
+    }
+
+    public static void main(String[] args) {
+
+        List<Object> listOfVechile = new ArrayList<>();
+        listOfVechile.add(new Object());
+        listOfVechile.add(new Object());
+        listOfVechile.add(new Object());
+
+        Consumer sanjay = new Consumer(listOfVechile);
+
+        sanjay.parkVechile(new ParkingLot(2), new ParkingLot(3)); //plots
+
+
+    }
+
+
+}
